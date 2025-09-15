0c9da922f3ad505762b7df9100f44351994140cb
unikortec <unikortec@gmail.com>
2025-09-15 17:53:12 -0300

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,27 +1,17 @@
 // js/firebase.js
-// Configuração Firebase (CDN ESM) para UNIKOR Portal
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getAuth }       from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-import { getAnalytics, isSupported } 
-                        from "https://www.gstatic.com/firebasejs/12.2.1/firebase-analytics.js";
 
-// Configuração do projeto UNIKOR (corrigido)
 export const firebaseConfig = {
   apiKey: "AIzaSyC12s4PvUWtNxOlShPc7zXlzq4XWqlVo2w",
   authDomain: "unikorapp.firebaseapp.com",
   projectId: "unikorapp",
-  storageBucket: "unikorapp.appspot.com",   // <- ajuste aqui
+  storageBucket: "unikorapp.appspot.com",
   messagingSenderId: "329806123621",
   appId: "1:329806123621:web:9aeff2f5947cd106cf2c8c",
-  measurementId: "G-WLVX3YK3EN"
+  // measurementId: "G-WLXV3YK3EN" // opcional; pode deixar comentado
 };
 
-// Inicializa Firebase
 export const app  = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
-// Analytics (só se suportado no navegador)
-isSupported().then((ok) => {
-  if (ok) getAnalytics(app);
-}).catch(()=>{});
+// (analytics removido por enquanto)
