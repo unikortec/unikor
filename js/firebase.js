cc59222c924b59a50ba1c3a88714c00e9d3fa6b9
unikortec <unikortec@gmail.com>
2025-09-15 16:23:51 -0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,16 +3,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebas
 import { getAuth }       from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyC12s4PvUWtNxOlShPc7zXlzq4XWqlVo2w",
+  apiKey: "AIzaSyC12s4PvUWtNxolShPc7zlXq4XWqlVo2w",
   authDomain: "unikorapp.firebaseapp.com",
   projectId: "unikorapp",
   storageBucket: "unikorapp.firebasestorage.app",
   messagingSenderId: "329806123621",
   appId: "1:329806123621:web:9aeff2f5947cd106cf2c8c",
-  measurementId: "G-WLXV3YK3EN"
+  measurementId: "G-WLVX3YK3EN"
 };
 
 export const app  = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-</script>
-
