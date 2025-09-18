6688d125aecc6b9331db0b3e9e6915b36db493ba
unikortec <unikortec@gmail.com>
2025-09-18 12:42:52 -0300

    Create sw.js

---
+++ app/pedidos/sw.js
@@ -0,0 +1,22 @@
+const CACHE = 'unikor-pedidos-v101';
+self.addEventListener('install', e=>{
+  e.waitUntil((async()=>{
+    const c = await caches.open(CACHE);
+    await c.addAll(['./','./index.html','./css/style.css','./js/app.js','./js/firebase.js','./js/calcular-entrega.js']);
+    self.skipWaiting();
+  })());
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil((async()=>{
+    const keys = await caches.keys();
+    await Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)));
+    self.clients.claim();
+  })());
+});
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin){
+    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
+  }
+});
+self.addEventListener('message', (e)=>{ if (e.data==='SKIP_WAITING') self.skipWaiting(); });
