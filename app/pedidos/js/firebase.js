2eaa17d411b9cfa0ccb5ddb0f9102fb91bdfeace
unikortec <unikortec@gmail.com>
2025-09-24 23:54:38 -0300

    Atualizar o firebase.js

--- app/pedidos/js/firebase.js
+++ app/pedidos/js/firebase.js
@@ -1,39 +1,30 @@
 // app/pedidos/js/firebase.js
-// Firestore + auth anônima (tenant Serra Nobre) para rodar o módulo isolado.
+// Firestore + auth anônima (tenant fixo Serra Nobre)
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-import {
-  getAuth, signInAnonymously, onAuthStateChanged, setPersistence, browserLocalPersistence
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-import {
-  getFirestore, enableIndexedDbPersistence
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
+import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-/* CONFIG DO PROJETO (seu print) */
-const firebaseConfig = {
+/* CONFIG DO PROJETO - MESMO DO PORTAL */
+export const firebaseConfig = {
   apiKey: "AIzaSyC12s4PvUWtNxOlShPc7zXlzq4XWqlVo2w",
   authDomain: "unikorapp.firebaseapp.com",
   projectId: "unikorapp",
-  storageBucket: "unikorapp.firebasestorage.app",
+  storageBucket: "unikorapp.appspot.com",
   messagingSenderId: "329806123621",
-  appId: "1:329806123621:web:9aeff2f5947cd106cf2c8c",
-  measurementId: "G-WLXV3YK3EN"
+  appId: "1:329806123621:web:9aeff2f5947cd106cf2c8c"
 };
 
 export const app = initializeApp(firebaseConfig);
 export const db  = getFirestore(app);
 
+// Auth anônima
 const auth = getAuth(app);
-await setPersistence(auth, browserLocalPersistence);
+export const authReady = new Promise((resolve) => onAuthStateChanged(auth, () => resolve(true)));
+signInAnonymously(auth).catch(()=>{});
 
-// Resolve quando tiver um usuário (se não tiver, entra anônimo)
-export const authReady = new Promise((resolve) => {
-  onAuthStateChanged(auth, (u) => resolve(!!u));
-});
-signInAnonymously(auth).catch(()=>{ /* silencioso */ });
-
-// Tenant fixo durante o piloto isolado
+// Tenant fixo (rodando isolado)
 export const TENANT_ID = "serranobrecarnes.com.br";
 
-// Offline
+// Persistência offline
 enableIndexedDbPersistence(db).catch(()=>{});
