41b53d42a29c3fbf99d59aef9f7c8330ba767613
unikortec <unikortec@gmail.com>
2025-09-25 20:27:14 -0300

    Atualizar o firebase.js

--- app/pedidos/js/firebase.js
+++ app/pedidos/js/firebase.js
@@ -1,11 +1,13 @@
 // app/pedidos/js/firebase.js
 // Firestore + auth anônima (tenant fixo Serra Nobre)
 
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getAuth, signInAnonymously, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 
-/* CONFIG DO PROJETO - MESMO DO PORTAL */
+
+/ CONFIG DO PROJETO - MESMO DO PORTAL /
 export const firebaseConfig = {
   apiKey: "AIzaSyC12s4PvUWtNxOlShPc7zXlzq4XWqlVo2w",
   authDomain: "unikorapp.firebaseapp.com",
@@ -15,22 +17,28 @@ export const firebaseConfig = {
   appId: "1:329806123621:web:9aeff2f5947cd106cf2c8c"
 };
 
+
 export const app = initializeApp(firebaseConfig);
 export const db  = getFirestore(app);
 
+
 // Auth anônima
 const auth = getAuth(app);
+
 
 // Só considera "pronto" quando já existir usuário (evita rodar queries sem credencial)
 export const authReady = new Promise((resolve) => {
   onAuthStateChanged(auth, (u) => { if (u) resolve(true); });
 });
 
+
 // dispara o login anônimo
 signInAnonymously(auth).catch(() => { /* silencioso */ });
 
+
 // Tenant fixo (rodando isolado)
 export const TENANT_ID = "serranobrecarnes.com.br";
+
 
 // Persistência offline do Firestore (ignora se o navegador não suportar)
 enableIndexedDbPersistence(db).catch(() => {});
