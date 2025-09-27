141c3bd679f63ebaed5e0b0e202561df329a150a
unikortec <unikortec@gmail.com>
2025-09-27 20:17:11 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* UNIKOR Portal – Service Worker (v1.0.3) */
-const CACHE = "unikor-portal-v1.0.7"; // 🔁 troque a cada deploy (ou injete hash de build)
+const CACHE = "unikor-portal-v1.0.8"; // 🔁 troque a cada deploy (ou injete hash de build)
 
 const CORE = [
   // Shell & PWA
