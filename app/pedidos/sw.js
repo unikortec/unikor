9c4c8b11e7c46f14277d81cca82775758428b460
unikortec <unikortec@gmail.com>
2025-10-03 22:00:38 -0300

    Atualizar o sw.js

--- app/pedidos/sw.js
+++ app/pedidos/sw.js
@@ -1,5 +1,5 @@
 // /app/pedidos/sw.js
-const APP_VER   = '1.2.3';   // 🔺 incrementado
+const APP_VER   = '1.2.4';   // 🔺 incrementado
 const TAG       = 'pedidos';
 const STATIC    = `${TAG}-static-${APP_VER}`;
 const OFFLINE   = './index.html';
