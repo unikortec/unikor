abae0fd483e0589dbdbd8fd72e29387946549b07
unikortec <unikortec@gmail.com>
2025-10-09 10:08:37 -0300

    Atualizar o sw.js

--- app/relatorios/sw.js
+++ app/relatorios/sw.js
@@ -1,5 +1,5 @@
 /* Unikor – Relatórios SW */
-const APP_VERSION = '1.1.7';
+const APP_VERSION = '1.1.8';
 const CACHE = `unikor-delatórios::${APP_VERSION}`;
 
 self.addEventListener('install', (event) => {
